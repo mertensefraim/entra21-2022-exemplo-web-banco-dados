9740041f8307711a15f833d05aecbf0c03cd4496
Efraim Mertens <mertensefraim@gmail.com>
2022-08-03 22:07:30 -0300

    Criado projeto inicial web sobre clinica veterinária

---
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entra21.CSharp.ClinicaVeterinaria.ClinicaVeterinaria.Aplicacao.Controllers
+{
+    public class RacaController : Controller
+    {
+        [Route("/raca")]
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            return View();
+        }
+    }
+}
